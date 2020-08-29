6fd369b6fb3dcd5c70b4b09003c036011010afae
SajalJain906 <sajal.jain906@gmail.com>
2020-08-29 17:18:09 +0530

    analysis file

--- analysis.R
+++ analysis.R
@@ -1,6 +1,3 @@
-data <- read.csv("repdata_data_StormData.csv.bz2")
-head(data)
-str(data)
 sum(is.na(data$EVTYPE))
 sum(is.na(data$FATALITIES))
 sum(is.na(data$INJURIES))
@@ -9,8 +6,15 @@ fatalities <- aggregate(FATALITIES~EVTYPE,data,sum)
 injuries <- aggregate(INJURIES~EVTYPE,data,sum)
 health_effect <- merge(fatalities,injuries,by="EVTYPE")
 health_effect <- health_effect[order(health_effect$FATALITIES,health_effect$INJURIES,decreasing = TRUE),]
-health_effect[1:5,]
+health_effect[1:20,]
 
 with(health_effect[1:5,],barplot(FATALITIES,col = 1:5,names.arg=EVTYPE))
 title("TOTAL FATALITIES IN US")
 
+econ_damage <- data$PROPDMG + data$CROPDMG
+data <- cbind(data,econ_damage)
+
+economic_damage <- economic_damage[order(economic_damage$econ_damage,decreasing = TRUE),]
+economic_damage[1:20,]
+with(economic_damage[1:5,],barplot(econ_damage,col=1:5,names.arg = EVTYPE))
+title("TOTAL ECONOMIC DAMAGE IN US")
